623cffb18fed250ac8a957fb7c9e90e3fccae571
Andressa Da Costa <andressadacostan@gmail.com>
2023-02-19 17:48:46 -0300

    ui-twitter

---
+++ README.md
@@ -0,0 +1,21 @@
+# <img src="./src/assets/logo-twitter.svg"> UI Twitter - React <img src="./src/assets/react-logo.svg">
+
+<img width="400" src="https://user-images.githubusercontent.com/2254731/219364698-e4281309-b996-4de9-8aa7-8db5df9370ba.png" />
+
+Tá afim de construir esse projeto do **ZERO**? Só acessar o link:
+
+> https://quiz.rocketseat.com.br/masterclass/react
+
+> https://www.figma.com/file/POsBury9FPMZ1j9qfFP6Hb/Twitter-UI-(Community)?node-id=204%3A27&t=OOD2CnCrFCUWjtuM-1
+
+> https://efficient-sloth-d85.notion.site/Quiz-Rocketseat-Masterclass-React-8098599ae785490a811f6245001a8308
+
+## Sobre
+
+Esse projeto é um clone da interface de usuário (UI) do Twitter utilizando React focado em abordar os fundamentos dessa tecnologia como componentização, propriedades, estado, formulários, etc.
+
+## Preview do projeto
+
+Você pode conferir o projeto funcionando aqui:
+
+> https://...
---
+++ src/pages/Timeline.tsx
@@ -0,0 +1,68 @@
+import { FormEvent, KeyboardEvent, useState } from 'react'
+import { Header } from '../components/Header'
+import { Separator } from '../components/Separator'
+import { Tweet } from '../components/Tweet'
+
+import './Timeline.css'
+
+export function Timeline() {
+	const [newTweet, setNewTweet] = useState('')
+	const [tweets, setTweets] = useState([
+		'Acabei de migrar um projeto React GIGANTE de create-react-app para Vite e os resultados foram: ✅ npm start: De 32s para 400ms (sim, demorava 30s) ✅ npm build: De 120s para 22s, Além disso, troquei do Yarn para o PNPM e o install das dps mudou de 24s para 8s 🔥',
+		'Estamos vivendo um momento interessante no mercado de startups. Soluções que resolvem problemas de forma semelhante, mas com precificação mais simples, usabilidade e foco em dev first. Tudo começa com um projeto open source/free 👀',
+		'Deu certo tweetar!'
+	])
+
+	function createNewTweet(event: FormEvent) {
+		event.preventDefault()
+
+		setTweets([newTweet, ...tweets])
+		setNewTweet('')
+	}
+
+	function handleHotkeySubmit(event: KeyboardEvent) {
+		if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+			setTweets([newTweet, ...tweets])
+			setNewTweet('')
+		}
+	}
+
+	return (
+		<main className="timeline">
+			<Header title="Home" />
+
+			<form
+				onSubmit={createNewTweet}
+				className="new-tweet-form">
+				<label htmlFor="tweet">
+					<img
+						src="https://github.com/diego3g.png"
+						alt="Diego Fernandes"
+					/>
+					<textarea
+						id="tweet"
+						placeholder="What's happening?"
+						value={newTweet}
+						onKeyDown={handleHotkeySubmit}
+						onChange={(event) => {
+							setNewTweet(event.target.value)
+						}}
+					/>
+				</label>
+
+				<button type="submit">Tweet</button>
+			</form>
+
+			<Separator />
+
+			{tweets.map((tweet) => {
+				return (
+					<Tweet
+						key={tweet}
+						content={tweet}
+					/>
+				)
+			})}
+		</main>
+	)
+}
